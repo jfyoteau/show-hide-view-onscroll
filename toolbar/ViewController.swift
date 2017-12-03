8b8e815c1abe43009f42e3eac07534ce9f9ce05f
ヨト・ジャンフランソワ <jf.yoteau@yoto-no-MacBook-Pro.local>
2017-12-03 10:32:55 +0900

    Initial Commit

---
+++ toolbar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  toolbar
+//
+//  Created by ヨト　ジャンフランソワ on 03/12/2017.
+//  Copyright © 2017 tcmobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
